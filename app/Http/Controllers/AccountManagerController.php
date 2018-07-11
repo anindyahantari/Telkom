68921dd2286ee1c64c1c265fda2eb732164f01cf
anindyahantari <anindyahantari@gmail.com>
2018-07-11 16:11:03 +0700

    Account Manager Controller + Middleware 2

--- app/Http/Controllers/AccountManagerController.php
+++ app/Http/Controllers/AccountManagerController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\AspekBisnis;
+use App\Jabatan;
+use App\LatarBelakang;
+use App\Mitra;
+use App\Pelanggan;
+use App\Proyek;
+use App\User;
+use DB;
 
 class AccountManagerController extends Controller
 {
-    //
+    public
 }
